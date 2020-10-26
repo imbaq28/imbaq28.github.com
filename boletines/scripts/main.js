d0a3e8bd4814652127d18d7fc3f4f7d3e9462fe7
imbaq28 <jfgosugank@gmail.com>
2020-10-26 17:38:30 -0400

    redireccion a boletines

--- boletines/scripts/main.js
+++ boletines/scripts/main.js
@@ -27,7 +27,7 @@ links.forEach(function(link){
 		content.classList.add("animate__animated");
 
 		setTimeout(function(){			
-			location.href = "/";
+			location.href = "/boletines";
 		},600)
 
 		
